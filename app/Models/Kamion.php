e5fa7440cda5cbdf89f2d780a2fb04d264d528c9
FilipMilovanovic <coficofi4@gmail.com>
2022-06-05 14:10:49 +0200

    Napravljena tri modela Prevoznik, Kamion i Tura

---
+++ app/Models/Kamion.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Prevoznik;
+use App\Models\Tura;
+
+class Kamion extends Model
+{
+    use HasFactory;
+
+    public function prevoznik()
+    {
+        return $this->belongsTo(Prevoznik::class);
+    }
+
+    public function ture()
+    {
+        return $this->hasMany(Tura::class);
+    }
+}
